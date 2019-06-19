a38bdaff41c3b6bdb6438d3529f62569571754fd
Vaishak Iyer <vaishak94@gmail.com>
2019-06-19 11:25:30 +0530

    Initial Commit

---
+++ ViperModelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperModelDemo
+//
+//  Created by Vaishak Iyer on 19/06/19.
+//  Copyright © 2019 Vaishak Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
